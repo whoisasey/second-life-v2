99340ba09a4b038c605076a848e142a9eab4007f
atina <atinachang@gmail.com>
2023-11-11 17:34:16 -0500

    fix env path

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const charityRoutes = require("./routes/charities");
