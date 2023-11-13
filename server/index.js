0bb779041853030f3d29891d20dcf8739f3278b4
atina <atinachang@gmail.com>
2023-11-12 19:48:18 -0500

    update cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const userRoutes = require("./routes/users");
 const app = express();
 
 //middleware
-app.use(cors());
+app.use(
+	cors({
+		origin: "https://second-life-v2.vercel.app/",
+		methods: ["GET", "POST", "PATCH", "DELETE"],
+		credentials: true,
+	}),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser());
