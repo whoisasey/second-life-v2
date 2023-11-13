a754c8040d2dc045adc01be82e84e4e432c581f9
atina <atinachang@gmail.com>
2023-11-12 20:27:20 -0500

    cleanup

--- server/index.js
+++ server/index.js
@@ -1,19 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieparser = require("cookie-parser");
-const cors = require("cors");
 const charityRoutes = require("./routes/charities");
 const userRoutes = require("./routes/users");
 // express app
 const app = express();
 
 //middleware
-// app.use(cors());
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieparser());
 
 app.use((req, res, next) => {
 	console.log(req.path, req.method);
@@ -34,7 +29,5 @@ mongoose
 		});
 	})
 	.catch((e) => console.log(e));
-
-// app.use(express.static("public"));
 
 module.exports = app;
