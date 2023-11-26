273666b049075d935bd7b45400397ca7cd18b689
atina <atinachang@gmail.com>
2023-11-26 18:05:40 -0500

    add react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import Charities from "./components/Charities";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
-		<>
+		<div className="app">
 			<h1>hello world</h1>
-			<Charities />
-		</>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/charities" element={<Charities />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
 
