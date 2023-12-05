86c8f31a66614b5fad95eb921dd59ee69221d795
atina <atinachang@gmail.com>
2023-12-04 19:49:55 -0500

    add sign up form

--- client/src/components/Charities/CharityForm/CharityForm.tsx
+++ client/src/components/Charities/CharityForm/CharityForm.tsx
@@ -1,8 +1,17 @@
 import { ChangeEvent, FormEvent, useState } from "react";
+import { charities } from "../../../utils/charities";
 interface FormData {
 	charityId: string;
 	name: string;
 	email: string;
+	password: string;
+}
+
+type Value = "id" | "name";
+
+interface ValueProps {
+	name: Value;
+	id: Value;
 }
 
 const CharityForm = () => {
@@ -10,18 +19,34 @@ const CharityForm = () => {
 		name: "",
 		charityId: "",
 		email: "",
+		password: "",
 	});
+	const [charityName, setCharityName] = useState<string>("");
 
 	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
+
 		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+		const { value } = e.target;
+
+		const getName = charities.find((ch) => ch.id === value);
+		setCharityName(getName.name);
+
+		setFormData({
+			...formData,
+			charityId: value,
+			name: getName.name,
+		});
 	};
 
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
 		try {
-			const res = await fetch("http://localhost:5500/api/charities/", {
+			const res = await fetch("http://localhost:5500/api/users/", {
 				method: "POST",
 				headers: {
 					Accept: "application/json",
@@ -31,6 +56,7 @@ const CharityForm = () => {
 					name: formData.name,
 					email: formData.email,
 					charityId: formData.charityId,
+					password: formData.password,
 				}),
 			});
 			const resJson = await res.json();
@@ -47,43 +73,58 @@ const CharityForm = () => {
 	};
 
 	return (
-		<form action="" onSubmit={handleSubmit}>
-			<div>
-				<label>
-					Charity Name:{" "}
-					<input
-						type="text"
-						name="name"
-						value={formData.name}
-						onChange={handleInputChange}
-					/>
-				</label>
-			</div>
-			<div>
-				<label>
-					Charity ID:
-					<input
-						type="number"
-						name="charityId"
-						value={formData.charityId}
-						onChange={handleInputChange}
-					/>
-					<br />* this will be a dropdown array of charities*
-				</label>
-			</div>
-			<div>
-				<label>
-					Charity Email:{" "}
-					<input
-						type="email"
-						name="email"
-						value={formData.email}
-						onChange={handleInputChange}
-					/>
-				</label>
-			</div>
-			<button type="submit">Submit</button>
-		</form>
+		<>
+			<form action="" onSubmit={handleSubmit}>
+				<div>
+					<label>
+						Charity:
+						<select
+							name={formData.charityId}
+							value={formData.charityId}
+							onChange={handleSelectChange}>
+							<option value="" disabled>
+								Select a Charity
+							</option>
+							{charities.map(({ id, name }: ValueProps) => (
+								<option value={id} key={id}>
+									{name}
+								</option>
+							))}
+						</select>
+					</label>
+				</div>
+				<div>
+					<label>
+						Charity Name:{" "}
+						<input type="text" name="name" value={charityName} readOnly />
+					</label>
+				</div>
+				<div>
+					<label>
+						Charity Email:{" "}
+						<input
+							type="email"
+							name="email"
+							value={formData.email}
+							onChange={handleInputChange}
+						/>
+					</label>
+				</div>
+				<div>
+					<label htmlFor="">
+						Password:{" "}
+						<input
+							type="password"
+							name="password"
+							placeholder="Please enter a password"
+							value={formData.password}
+							onChange={handleInputChange}
+						/>
+					</label>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</>
 	);
 };
 
---
+++ client/src/utils/charities.js
@@ -0,0 +1,7 @@
+export const charities = [
+	{ id: "1", name: "Jessie's - The June Callwood Center for Women" },
+	{ id: "2", name: "The Salvation Army" },
+	{ id: "3", name: "Toronto Humane Society" },
+	{ id: "4", name: "Regent Park Community Health Centre" },
+	{ id: "5", name: "Covenant House Toronto" },
+];
