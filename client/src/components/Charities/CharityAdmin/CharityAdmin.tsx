5db20923e1d176bf36af489c3ad4745886b1eb44
atina <atinachang@gmail.com>
2023-12-17 21:25:48 -0500

    charity admin page

--- client/src/components/Charities/CharityAdmin/CharityAdmin.tsx
+++ client/src/components/Charities/CharityAdmin/CharityAdmin.tsx
@@ -1,18 +1,17 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 type PropsType = {
   user: string;
-  loggedIn: boolean;
 };
 
-const CharityAdmin = ({ user, loggedIn }: PropsType) => {
-  if (!loggedIn) {
-    return (
-      <>
-        <h2>Sorry, you are not authorized to view this page!</h2>
-        <Link to="/charities/signin">Go Back</Link>
-      </>
-    );
-  }
+const CharityAdmin = ({ user }: PropsType) => {
+  // if (!loggedIn) {
+  //   return (
+  //     <>
+  //       <h2>Sorry, you are not authorized to view this page!</h2>
+  //       <Link to="/charities/signin">Go Back</Link>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
