4f107976514b100162d449b3a8b79a178ac79b8f
atina <atinachang@gmail.com>
2023-12-17 21:25:01 -0500

    add state context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { UserStateProvider } from "./context/UserStateContext";
 import { Layout } from "./components/Layout/Layout";
 import {
   Charities,
@@ -11,27 +12,27 @@ import Blurb from "./utils/Blurb";
 
 function App() {
   const [user, setUser] = useState<string>("");
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   return (
-    <Layout>
-      <main>
-        <Routes>
-          <Route path="/" element={<Blurb />} />
-          <Route path="/charities" element={<Charities />} />
-          <Route path="/charities/login" element={<LoginOrCreate />} />
-          <Route
-            path="/charities/signin"
-            element={<SignIn setUser={setUser} setLoggedIn={setLoggedIn} />}
-          />
-
-          <Route
-            path="/charities/admin/:id"
-            element={<CharityAdmin user={user} loggedIn={loggedIn} />}
-          />
-        </Routes>
-      </main>
-    </Layout>
+    <UserStateProvider>
+      <Layout>
+        <main>
+          <Routes>
+            <Route path="/" element={<Blurb />} />
+            <Route path="/charities" element={<Charities />} />
+            <Route path="/charities/login" element={<LoginOrCreate />} />
+            <Route
+              path="/charities/signin"
+              element={<SignIn setUser={setUser} />}
+            />
+            <Route
+              path="/charities/admin/:id"
+              element={<CharityAdmin user={user} />}
+            />
+          </Routes>
+        </main>
+      </Layout>
+    </UserStateProvider>
   );
 }
 
--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,17 +1,40 @@
 import { NavLink } from "react-router-dom";
 import styled from "./Nav.module.scss";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { UserStateContext } from "../../context/UserStateContext";
 
 const Nav = () => {
-	return (
-		<nav className={styled.nav}>
-			<NavLink to="/">Second Life</NavLink>
+  // const [token, setToken] = useState<string>();
+  const { setLoggedIn } = useContext(UserStateContext);
 
-			<div className={styled.menu}>
-				<NavLink to="/charities">Charities</NavLink>
-				<NavLink to="/charities/login">Register as a Charity</NavLink>
-			</div>
-		</nav>
-	);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    console.log(user);
+
+    if (user.token) {
+      setLoggedIn(true);
+      // setToken(user.id);
+    }
+    // console.log(token);
+  }, []);
+
+  return (
+    <nav className={styled.nav}>
+      <NavLink to="/">Second Life</NavLink>
+
+      <div className={styled.menu}>
+        <NavLink to="/charities">Charities</NavLink>
+        {/* {loggedIn ? (
+					<NavLink to={`/charities/admin/${token}`}>
+						Go to My Charity Page
+					</NavLink>
+				) : ( */}
+        <NavLink to="/charities/login">Register as a Charity</NavLink>
+        {/* )} */}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
