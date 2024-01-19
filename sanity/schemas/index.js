5ec6164991b7c95ba4b0092fd11611c21f1e427c
atina <atinachang@gmail.com>
2024-01-18 21:17:43 -0500

    add sanity CMS

---
+++ sanity/schemas/index.js
@@ -0,0 +1,2 @@
+import page from './page'
+export const schemaTypes = [page]
---
+++ sanity/schemas/page.js
@@ -0,0 +1,20 @@
+export default {
+  name: 'page',
+  type: 'document',
+  title: 'Page',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    },
+  ],
+}
