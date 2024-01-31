d77c5453cd476baa3bff8bd795082c1e583fdac5
atina <atinachang@gmail.com>
2024-01-31 14:36:18 -0500

    fix eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,32 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["standard-with-typescript", "plugin:react/recommended"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		"@typescript-eslint/quotes": 1,
+		"@typescript-eslint/semi": "warn",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"no-tabs": "warn",
+		"react/react-in-jsx-scope": "off",
+	},
+};
--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 // import { useEffect, useContext } from "react";
 // import { UserStateContext } from "../../context/UserStateContext";
